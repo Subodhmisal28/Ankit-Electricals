1f11207b9e3487a743f98b410b69b377e0ca880b
Subodhmisal28 <110788130+Subodhmisal28@users.noreply.github.com>
2025-03-05 17:38:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("alertButton").addEventListener("click", function() {
+        alert("Welcome to Ankit Electricals!");
+    });
+});
